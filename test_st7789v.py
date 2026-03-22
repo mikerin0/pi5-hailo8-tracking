4cad081ee40747d27ea40579bde9f26cd0180f4b
mikerin0 <mike@mikerino.com>
2026-03-22 11:19:56 -0400

    5

--- test_st7789v.py
+++ test_st7789v.py
@@ -7,12 +7,11 @@ serial = spi(port=0, device=0, gpio_DC=25, gpio_RST=27, gpio_LIGHT=18)
 device = st7789(serial, width=240, height=320, rotate=0)
 
 bar_width = 20
-steps = device.width - bar_width
-for x in range(0, steps, 5):
+for x in range(0, device.width, 2):
 	image = Image.new("RGB", (device.width, device.height), (0, 0, 0))
 	draw = ImageDraw.Draw(image)
-	draw.rectangle([x, 0, x + bar_width, device.height], fill=(0, 255, 0))
+	x1 = min(x + bar_width, device.width)
+	draw.rectangle([x, 0, x1, device.height], fill=(0, 255, 0))
 	device.display(image)
-	time.sleep(0.03)
-
+	time.sleep(0.02)
 time.sleep(1)
