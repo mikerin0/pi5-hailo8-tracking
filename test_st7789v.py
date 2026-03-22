b86fd8c90efaafb28d3aea7dd0e864dffd51bb96
mikerin0 <mike@mikerino.com>
2026-03-22 11:08:27 -0400

    2

--- test_st7789v.py
+++ test_st7789v.py
@@ -4,12 +4,20 @@ from PIL import Image, ImageDraw
 import time
 
 serial = spi(port=0, device=0, gpio_DC=25, gpio_RST=27, gpio_LIGHT=18)
-device = st7789(serial, width=240, height=135, rotate=0)
+device = st7789(serial, width=240, height=320, rotate=0)
 
 image = Image.new("RGB", (device.width, device.height), (0, 0, 0))
 draw = ImageDraw.Draw(image)
-draw.rectangle([20, 20, device.width-20, device.height-20], outline="yellow", width=5)
-draw.text((60, 60), "ST7789V OK!", fill="white")
+
+box_width = 160
+box_height = 120
+x0 = (device.width - box_width) // 2
+y0 = (device.height - box_height) // 2
+x1 = x0 + box_width
+y1 = y0 + box_height
+
+draw.rectangle([x0, y0, x1, y1], outline="yellow", width=5)
+draw.text((x0 + 20, y0 + 50), "ST7789V OK!", fill="white")
 
 device.display(image)
-time.sleep(10)  # Image remains visible for 10 seconds
+time.sleep(10)
