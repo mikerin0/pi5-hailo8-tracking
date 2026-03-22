380415b87617b45782cb15664b92b71686ca0655
mikerin0 <mike@mikerino.com>
2026-03-22 11:26:17 -0400

    7

--- test_st7789v.py
+++ test_st7789v.py
@@ -6,12 +6,15 @@ import time
 serial = spi(port=0, device=0, gpio_DC=25, gpio_RST=27, gpio_LIGHT=18)
 device = st7789(serial, width=320, height=240, rotate=0)
 
-bar_width = 20
-for x in range(0, device.width, 2):
-	image = Image.new("RGB", (device.width, device.height), (0, 0, 0))
-	draw = ImageDraw.Draw(image)
-	x1 = min(x + bar_width, device.width)
-	draw.rectangle([x, 0, x1, device.height], fill=(0, 255, 0))
-	device.display(image)
-	time.sleep(0.02)
-time.sleep(1)
+image = Image.new("RGB", (device.width, device.height), (0, 0, 0))
+draw = ImageDraw.Draw(image)
+
+# Draw orientation text in each corner and center
+draw.text((5, 5), "TOP LEFT", fill="red")
+draw.text((device.width - 80, 5), "TOP RIGHT", fill="green")
+draw.text((5, device.height - 20), "BOTTOM LEFT", fill="blue")
+draw.text((device.width - 110, device.height - 20), "BOTTOM RIGHT", fill="yellow")
+draw.text((device.width // 2 - 40, device.height // 2 - 10), "CENTER", fill="white")
+
+device.display(image)
+time.sleep(10)
