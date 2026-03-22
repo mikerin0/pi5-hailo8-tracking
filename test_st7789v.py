6f01a798806f36a191cd87d3c9e4e4cdb8c4a17b
mikerin0 <mike@mikerino.com>
2026-03-22 11:18:11 -0400

    4

--- test_st7789v.py
+++ test_st7789v.py
@@ -1,16 +1,18 @@
 from luma.core.interface.serial import spi
 from luma.lcd.device import st7789
-from PIL import Image
+from PIL import Image, ImageDraw
 import time
 
 serial = spi(port=0, device=0, gpio_DC=25, gpio_RST=27, gpio_LIGHT=18)
 device = st7789(serial, width=240, height=320, rotate=0)
 
-colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 255), (0, 0, 0)]  # Red, Green, Blue, White, Black
-color_names = ["Red", "Green", "Blue", "White", "Black"]
-
-for i, color in enumerate(colors):
-	image = Image.new("RGB", (device.width, device.height), color)
+bar_width = 20
+steps = device.width - bar_width
+for x in range(0, steps, 5):
+	image = Image.new("RGB", (device.width, device.height), (0, 0, 0))
+	draw = ImageDraw.Draw(image)
+	draw.rectangle([x, 0, x + bar_width, device.height], fill=(0, 255, 0))
 	device.display(image)
-	print(f"Displaying {color_names[i]}")
-	time.sleep(2)
+	time.sleep(0.03)
+
+time.sleep(1)
